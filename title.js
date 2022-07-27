0c2649b36c21b1e8d8a5de78b05fee65f9b72a76
hach <72147745+hcrk9@users.noreply.github.com>
2022-07-27 11:25:40 +0800

    Add files via upload

---
+++ title.js
@@ -0,0 +1,14 @@
+setInterval(function () {
+    const show = document.querySelector('span[data-show]')
+    const next = show.nextElementSibling || document.querySelector('span[first]')
+    const up = document.querySelector('span[data-up]')
+
+    if (up) {
+        up.removeAttribute('data-up')
+    }
+
+    show.removeAttribute('data-show')
+    show.setAttribute('data-up', '')
+
+    next.setAttribute('data-show', '')
+}, 2000)
